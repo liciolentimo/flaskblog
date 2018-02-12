9fa0bad7bacdb811b726b261dcad56ac86bea69f
Licio Lentimo <lentimo@gmail.com>
2018-02-12 23:31:35 +0300

    Added code for login and access control

---
+++ app2.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from data import Articles
+from flask_mysqldb import MySQL 
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+app = Flask(__name__)
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init mysql
+mysql = MySQL(app)
+
+Articles = Articles()
+
+@app.route('/')
+def index(): 
+	return render_template('home.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+	return render_template('articles.html',articles= Articles)		
+
+@app.route('/article/<string:id>/')
+def article(id):
+	return render_template('article.html',id=id)	
+
+class RegisterForm(Form):
+	name = StringField('Name', [validators.Length(min=1,max=50)])
+	username = StringField('Username', [validators.Length(min=4,max=25)])
+	email = StringField('Email', [validators.Length(min=4,max=50)])
+	password = PasswordField('Password', [
+		  validators.DataRequired(),
+		  validators.EqualTo('confirm', message='Passwords do not match')
+		])
+	confirm = PasswordField('Confirm Password')
+
+app.route('/register', methods=['GET', 'POST'])	
+def register():
+    form = RegisterForm(request.form)   
+    if request.method == 'POST' and form.validate():
+       name = form.name.data
+       email = form.email.data
+       username = form.username.data
+       password = sha256_crypt.encrypt(str(form.password.data))
+
+       # create cursor
+       cur = mysql.connection.cursor()
+
+       cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",(name, email, username, password))	
+
+       # commit to DB
+       mysql.connection.commit()
+
+       # close connection
+       cur.close()
+
+       flash('You are now registered and can log in', 'success')
+
+    return redirect(url_for('login'))
+    return render_template('register.html', form=form)	
+
+# user login
+app.route('/login', methods=['GET','POST'])    
+def login():
+	if request.method == 'POST':
+		#get form fields
+		username = request.form['username']
+		password_candidate = request.form['password']
+
+		# create cursor
+		cur = mysql.connection.cursor()
+
+		# get user by username
+		result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+		if result > 0:
+			#get stored hash
+			data = cur.fetchone()
+			password = data['password']
+
+			# compare passwords
+			if sha256_crypt.verify(password_candidate, password):
+			   # Passed
+			   session['logged_in'] = True
+			   session['username'] = username
+
+			   flash('You are now logged in', 'success')
+			   return redirect(url_for('dashboard'))
+
+			else:
+			   error = 'Invalid login'	
+		       return render_template('login.html', error=error)
+		    # close connection   
+		    cur.close()   		
+		else:
+		    error = 'Username not found'	
+		    return render_template('login.html', error=error)	
+		
+	return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')		
+
+if __name__ == '__main__':
+	app.secret_key='secret123'
+	app.run(debug=True)
+	
---
+++ data.py
@@ -0,0 +1,29 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article One',
+			'body': 'Andela’s business model and geographic positioning means an eclectic but dedicated list of primary investors. Last year it raised a seed round for an undisclosed sum that drew participation from investors such as the African Angel Network, Former America Online CEO Steve Case, Facebook alum and New Republic Owner Chris Hughes, social investment fund the Omidyar Network (ON), and even the basketball star Carmelo Anthony.',
+			'author': 'Licio Lentimo',
+			'create_date': '01-22-2018'
+
+		},
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'Andela’s business model and geographic positioning means an eclectic but dedicated list of primary investors. Last year it raised a seed round for an undisclosed sum that drew participation from investors such as the African Angel Network, Former America Online CEO Steve Case, Facebook alum and New Republic Owner Chris Hughes, social investment fund the Omidyar Network (ON), and even the basketball star Carmelo Anthony.',
+			'author': 'John Doe',
+			'create_date': '01-22-2018'
+
+		},
+		{
+			'id': 3,
+			'title': 'Article Three',
+			'body': 'Andela’s business model and geographic positioning means an eclectic but dedicated list of primary investors. Last year it raised a seed round for an undisclosed sum that drew participation from investors such as the African Angel Network, Former America Online CEO Steve Case, Facebook alum and New Republic Owner Chris Hughes, social investment fund the Omidyar Network (ON), and even the basketball star Carmelo Anthony.',
+			'author': 'Licio Lentimo',
+			'create_date': '01-22-2018'
+
+		}
+		
+	]
+	return articles
